b8a05b2fe0f6824a4ef5f89bffba8e2cc3c78d83
Rodrigo <rhbon.08@gmail.com>
2023-09-20 18:51:52 -0500

    Created footer and about

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+const Blog = () => {
+  return (
+    <Layout pagina="Blog">
+      <h1 className="heading">Este es el blog</h1>
+    </Layout>
+  );
+};
+
+export default Blog;
---
+++ src/pages/tienda.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+const Tienda = () => {
+  return (
+    <Layout pagina="Tienda Virtual">
+      <h1 className="heading">Esta es la Tienda</h1>
+    </Layout>
+  );
+};
+
+export default Tienda;
