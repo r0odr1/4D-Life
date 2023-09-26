a6dd359c738b5fe1e0cc946592e075021ab22f31
Rodrigo <rhbon.08@gmail.com>
2023-09-26 00:20:43 -0500

    create strapi

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,26 @@
 import Layout from "@/components/Layout";
 
-const Blog = () => {
+const Blog = ({ entradas }) => {
+  console.log(entradas);
   return (
     <Layout pagina="Blog">
       <h1 className="heading">Este es el blog</h1>
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  const url = "http://localhost:1337/api/blogs";
+  const respuesta = await fetch(url);
+  const entradas = await respuesta.json();
+
+  console.log(entradas);
+
+  return {
+    props: {
+      entradas,
+    },
+  };
+}
 
 export default Blog;
